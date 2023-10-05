6840ca7a314b269f115df9d78fa1d9caf4cf02ee
MIDLART <145492479+MIDLART@users.noreply.github.com>
2023-10-06 00:04:48 +0300

    Update task1.pl

--- task1.pl
+++ task1.pl
@@ -1,7 +1,28 @@
-% Первая часть задания - предикаты работы со списками
+% РџРµСЂРІР°СЏ С‡Р°СЃС‚СЊ Р·Р°РґР°РЅРёСЏ - РїСЂРµРґРёРєР°С‚С‹ СЂР°Р±РѕС‚С‹ СЃРѕ СЃРїРёСЃРєР°РјРё
 
-% Ниже - пример кода, который вы можете удалить.
+my_length([],0).
+my_length([_|T], L) :-
+  my_length(T, L1),
+  L is L1 + 1.
 
-mylen([],0).
-mylen([X|T],N) :- mylen(T,N1), N is N1+1.
+my_member(X,[X|_]).
+my_member(X,[_|T]) :-
+  my_member(X,T).
+
+my_append([],L,L).
+my_append([H|T],L,[H|R]) :-
+  my_append(T,L,R).
+
+my_remove(X,[X|T],T).
+my_remove(X,[H|T],[H|Y]) :-
+  my_remove(X,T,Y).
+
+my_permute([],[]).
+my_permute(X,[H|T1]) :-
+  my_remove(H,X,T),
+  my_permute(T,T1).
+
+my_sublist(S,R) :-
+  my_append(R,_,S),
+  my_append(_,R,L).
 
