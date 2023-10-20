d3fefaeb54d79a8148a559bc044eef426a616891
MIDLART <145492479+MIDLART@users.noreply.github.com>
2023-10-20 21:14:37 +0300

    Update task2.pl

--- task2.pl
+++ task2.pl
@@ -3,4 +3,23 @@
 % The line below imports the data
 :- ['three.pl'].
 
+sum([],0). 
+sum([grade(_,Mark)|T],Sum):-
+    sum(T,Sum1), 
+    Sum is Sum1 + Mark.
+
 %Для каждого студента, найти средний балл, и сдал ли он экзамены или нет
+
+%Средний балл
+
+average_mark(Student, Average):-
+    student(_,Student,Marks),
+    sum(Marks,Sum),
+    length(Marks,Len),
+    Average is Sum/Len.
+
+%Сдал ли экзамены
+
+passed(Student):-
+    student(_,Student,Marks),
+    not(member(grade(_,2),Marks)).
